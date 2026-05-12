Shl1015CS/ArceOS

feat[rust-std]: add support for network

ulib/arceos-rust/lib/src/interface/io.rs
@@ -1,0 +1,21 @@
+use arceos_api::modules::axlog::info;
+use arceos_posix_api::ctypes::iovec;
+
+#[unsafe(no_mangle)]
+pub fn sys_write(fd: i32, buf: *const u8, count: usize) -> isize {
+    info!("called sys_write");
+    arceos_posix_api::sys_write(fd, buf as _, count)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_writev(fd: i32, iov: *const iovec, iovcnt: usize) -> isize {
+    info!("called sys_writev");
+    unsafe { arceos_posix_api::sys_writev(fd, iov, iovcnt as _) }
+}
+
+#[cfg(feature = "fd")]
+#[unsafe(no_mangle)]
+pub fn sys_close(fd: i32) -> i32 {
+    info!("called sys_close");
+    arceos_posix_api::sys_close(fd) as _
+}

ulib/arceos-rust/lib/src/interface/mem.rs
@@ -1,0 +1,32 @@
+use core::alloc::Layout;
+use arceos_api::modules::axlog::{error, info};
+
+#[unsafe(no_mangle)]
+pub fn sys_malloc(size: usize, align: usize) -> *mut u8 {
+    info!("called sys_malloc with size {} and align {}", size, align);
+    if let Ok(layout) = Layout::from_size_align(size, align) {
+        unsafe { alloc::alloc::alloc(layout) }
+    } else {
+        core::ptr::null_mut()
+    }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_free(ptr: *mut u8, size: usize, align: usize) {
+    info!("called sys_free");
+    if let Ok(layout) = Layout::from_size_align(size, align) {
+        unsafe { alloc::alloc::dealloc(ptr, layout) }
+    } else {
+        error!("sys_free called with invalid layout: size {}, align {}", size, align);
+    }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_realloc(ptr: *mut u8, size: usize, align: usize, new_size: usize) -> *mut u8 {
+    info!("called sys_realloc");
+    if let Ok(layout) = Layout::from_size_align(size, align) {
+        unsafe { alloc::alloc::realloc(ptr, layout, new_size) }
+    } else {
+        core::ptr::null_mut()
+    }
+}

ulib/arceos-rust/lib/src/interface/net.rs
@@ -1,0 +1,120 @@
+use arceos_api::modules::axlog::info;
+use arceos_posix_api::ctypes::{addrinfo, sockaddr, socklen_t, AF_INET, AF_INET6};
+use core::ffi::{c_char, c_void};
+
+fn convert_family(family: i32) -> i32 {
+    match family {
+        3 => AF_INET as _,
+        1 => AF_INET6 as _,
+        other => other,
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct HermitAddrInfo {
+    pub ai_flags: i32,
+    pub ai_family: i32,
+    pub ai_socktype: i32,
+    pub ai_protocol: i32,
+    pub ai_addrlen: socklen_t,
+    pub ai_canonname: *mut c_char,
+    pub ai_addr: *mut sockaddr,
+    pub ai_next: *mut addrinfo,
+}
+
+impl From<HermitAddrInfo> for addrinfo {
+    fn from(info: HermitAddrInfo) -> Self {
+        addrinfo {
+            ai_flags: info.ai_flags,
+            ai_family: convert_family(info.ai_family),
+            ai_socktype: info.ai_socktype,
+            ai_protocol: info.ai_protocol,
+            ai_addrlen: info.ai_addrlen,
+            ai_canonname: info.ai_canonname,
+            ai_addr: info.ai_addr,
+            ai_next: info.ai_next,
+        }
+    }
+}
+
+impl Into<HermitAddrInfo> for addrinfo {
+    fn into(self) -> HermitAddrInfo {
+        HermitAddrInfo {
+            ai_flags: self.ai_flags,
+            ai_family: match self.ai_family as u32 {
+                AF_INET => 3,
+                AF_INET6 => 1,
+                other => other as i32,
+            },
+            ai_socktype: self.ai_socktype,
+            ai_protocol: self.ai_protocol,
+            ai_addrlen: self.ai_addrlen,
+            ai_canonname: self.ai_canonname,
+            ai_addr: self.ai_addr,
+            ai_next: self.ai_next,
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_freeaddrinfo(addr_info: *mut HermitAddrInfo) {
+    let mut info = addrinfo::from(unsafe { *addr_info });
+    info!("[sys_freeaddrinfo] addr_info: {:?}", info);
+    unsafe { arceos_posix_api::sys_freeaddrinfo(&mut info) }
+    unsafe { *addr_info = info.into(); }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_getaddrinfo(
+    nodename: *const c_char,
+    servname: *const c_char,
+    hints: *const HermitAddrInfo,
+    res: *mut *mut HermitAddrInfo,
+) -> i32 {
+    let hints = if hints.is_null() {
+        core::ptr::null()
+    } else {
+        let h: addrinfo = unsafe { (*hints).into() };
+        &h as *const addrinfo
+    };
+    info!("[sys_getaddrinfo] nodename: {:?}, servname: {:?}", nodename, servname);
+    unsafe { arceos_posix_api::sys_getaddrinfo(nodename, servname, hints, res) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_send(s: i32, mem: *const c_void, len: usize, flags: i32) -> isize {
+    info!("[sys_send] socket: {}, len: {}, flags: {}", s, len, flags);
+    arceos_posix_api::sys_send(s, mem, len, flags)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_socket(domain: i32, type_: i32, protocol: i32) -> i32 {
+    info!(
+        "[sys_socket] domain: {}, type: {}, protocol: {}",
+        domain, type_, protocol
+    );
+    let domain = convert_family(domain);
+    arceos_posix_api::sys_socket(domain, type_, protocol)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_connect(socket_fd: i32, name: *const sockaddr, namelen: socklen_t) -> i32 {
+    info!(
+        "[sys_connect] socket_fd: {}, namelen: {}",
+        socket_fd, namelen
+    );
+    info!("name: {:?}", unsafe { *name });
+    let mut name_converted = unsafe { *name };
+    name_converted.sa_family = convert_family((name_converted.sa_family >> 8) as _) as _;
+    arceos_posix_api::sys_connect(socket_fd, &name_converted, namelen)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_recv(socket: i32, buf: *mut u8, len: usize, flags: i32) -> isize {
+    info!(
+        "[sys_recv] socket: {}, len: {}, flags: {}",
+        socket, len, flags
+    );
+    arceos_posix_api::sys_recv(socket, buf as _, len, flags)
+}

ulib/arceos-rust/lib/src/lib.rs
@@ -2,8 +2,9 @@
 extern crate alloc;
 
 mod syscall;
+mod interface;
 
-use arceos_api::modules::axlog::{ax_println, debug, info};
+use arceos_api::modules::axlog::{debug, info};
 pub use axruntime;
 
 unsafe extern "C" {

ulib/arceos-rust/lib/src/syscall.rs
@@ -1,9 +1,7 @@
-use alloc::alloc;
 use arceos_api::modules::axhal::time::wall_time_nanos;
-use arceos_api::modules::axlog::{ax_println, error, info};
+use arceos_api::modules::axlog::{ax_println, info};
 use arceos_api::sys::ax_terminate;
-use arceos_posix_api::ctypes::{iovec, timespec};
-use core::alloc::Layout;
+use arceos_posix_api::ctypes::timespec;
 use rand::rngs::SmallRng;
 use rand::{RngCore, SeedableRng};
 
@@ -28,36 +26,6 @@ pub fn sys_futex_wake(address: *mut u32, count: i32) -> i32 {
 }
 
 #[unsafe(no_mangle)]
-pub fn sys_malloc(size: usize, align: usize) -> *mut u8 {
-    info!("called sys_malloc with size {} and align {}", size, align);
-    if let Ok(layout) = Layout::from_size_align(size, align) {
-        unsafe { alloc::alloc(layout) }
-    } else {
-        core::ptr::null_mut()
-    }
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_free(ptr: *mut u8, size: usize, align: usize) {
-    info!("called sys_free");
-    if let Ok(layout) = Layout::from_size_align(size, align) {
-        unsafe { alloc::dealloc(ptr, layout) }
-    } else {
-        error!("sys_free called with invalid layout: size {}, align {}", size, align);
-    }
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_realloc(ptr: *mut u8, size: usize, align: usize, new_size: usize) -> *mut u8 {
-    info!("called sys_realloc");
-    if let Ok(layout) = Layout::from_size_align(size, align) {
-        unsafe { alloc::realloc(ptr, layout, new_size) }
-    } else {
-        core::ptr::null_mut()
-    }
-}
-
-#[unsafe(no_mangle)]
 pub fn sys_abort() -> ! {
     info!("called sys_abort");
     ax_terminate()
@@ -68,19 +36,6 @@ pub fn sys_exit(code: i32) -> ! {
     info!("called sys_exit with code {}", code);
     ax_println!("[ArceOS] Process exited with code {}", code);
     ax_terminate()
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_write(fd: i32, buf: *const u8, count: usize) -> isize {
-    info!("called sys_write");
-    arceos_posix_api::sys_write(fd, buf as _, count)
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_writev(fd: i32, iov: *const iovec, iovcnt: usize) -> isize {
-    // Placeholder implementation
-    info!("called sys_writev");
-    0
 }
 
 #[unsafe(no_mangle)]

LorenzLorentz/tgoskits

os/arceos/ulib/arceos-rust/lib/src/interface/io.rs
@@ -1,0 +1,21 @@
+use ax_api::modules::ax_log::info;
+use ax_posix_api::ctypes::iovec;
+
+#[unsafe(no_mangle)]
+pub fn sys_write(fd: i32, buf: *const u8, count: usize) -> isize {
+    info!("called sys_write");
+    ax_posix_api::sys_write(fd, buf as _, count)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_writev(fd: i32, iov: *const iovec, iovcnt: usize) -> isize {
+    info!("called sys_writev");
+    unsafe { ax_posix_api::sys_writev(fd, iov, iovcnt as _) }
+}
+
+#[cfg(feature = "fd")]
+#[unsafe(no_mangle)]
+pub fn sys_close(fd: i32) -> i32 {
+    info!("called sys_close");
+    ax_posix_api::sys_close(fd) as _
+}

os/arceos/ulib/arceos-rust/lib/src/interface/mem.rs
@@ -1,0 +1,32 @@
+use core::alloc::Layout;
+use ax_api::modules::ax_log::{error, info};
+
+#[unsafe(no_mangle)]
+pub fn sys_malloc(size: usize, align: usize) -> *mut u8 {
+    info!("called sys_malloc with size {} and align {}", size, align);
+    if let Ok(layout) = Layout::from_size_align(size, align) {
+        unsafe { alloc::alloc::alloc(layout) }
+    } else {
+        core::ptr::null_mut()
+    }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_free(ptr: *mut u8, size: usize, align: usize) {
+    info!("called sys_free");
+    if let Ok(layout) = Layout::from_size_align(size, align) {
+        unsafe { alloc::alloc::dealloc(ptr, layout) }
+    } else {
+        error!("sys_free called with invalid layout: size {}, align {}", size, align);
+    }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_realloc(ptr: *mut u8, size: usize, align: usize, new_size: usize) -> *mut u8 {
+    info!("called sys_realloc");
+    if let Ok(layout) = Layout::from_size_align(size, align) {
+        unsafe { alloc::alloc::realloc(ptr, layout, new_size) }
+    } else {
+        core::ptr::null_mut()
+    }
+}

os/arceos/ulib/arceos-rust/lib/src/interface/net.rs
@@ -1,0 +1,120 @@
+use ax_api::modules::ax_log::info;
+use ax_posix_api::ctypes::{addrinfo, sockaddr, socklen_t, AF_INET, AF_INET6};
+use core::ffi::{c_char, c_void};
+
+fn convert_family(family: i32) -> i32 {
+    match family {
+        3 => AF_INET as _,
+        1 => AF_INET6 as _,
+        other => other,
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct HermitAddrInfo {
+    pub ai_flags: i32,
+    pub ai_family: i32,
+    pub ai_socktype: i32,
+    pub ai_protocol: i32,
+    pub ai_addrlen: socklen_t,
+    pub ai_canonname: *mut c_char,
+    pub ai_addr: *mut sockaddr,
+    pub ai_next: *mut addrinfo,
+}
+
+impl From<HermitAddrInfo> for addrinfo {
+    fn from(info: HermitAddrInfo) -> Self {
+        addrinfo {
+            ai_flags: info.ai_flags,
+            ai_family: convert_family(info.ai_family),
+            ai_socktype: info.ai_socktype,
+            ai_protocol: info.ai_protocol,
+            ai_addrlen: info.ai_addrlen,
+            ai_canonname: info.ai_canonname,
+            ai_addr: info.ai_addr,
+            ai_next: info.ai_next,
+        }
+    }
+}
+
+impl Into<HermitAddrInfo> for addrinfo {
+    fn into(self) -> HermitAddrInfo {
+        HermitAddrInfo {
+            ai_flags: self.ai_flags,
+            ai_family: match self.ai_family as u32 {
+                AF_INET => 3,
+                AF_INET6 => 1,
+                other => other as i32,
+            },
+            ai_socktype: self.ai_socktype,
+            ai_protocol: self.ai_protocol,
+            ai_addrlen: self.ai_addrlen,
+            ai_canonname: self.ai_canonname,
+            ai_addr: self.ai_addr,
+            ai_next: self.ai_next,
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_freeaddrinfo(addr_info: *mut HermitAddrInfo) {
+    let mut info = addrinfo::from(unsafe { *addr_info });
+    info!("[sys_freeaddrinfo] addr_info: {:?}", info);
+    unsafe { ax_posix_api::sys_freeaddrinfo(&mut info) }
+    unsafe { *addr_info = info.into(); }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_getaddrinfo(
+    nodename: *const c_char,
+    servname: *const c_char,
+    hints: *const HermitAddrInfo,
+    res: *mut *mut HermitAddrInfo,
+) -> i32 {
+    let hints = if hints.is_null() {
+        core::ptr::null()
+    } else {
+        let h: addrinfo = unsafe { (*hints).into() };
+        &h as *const addrinfo
+    };
+    info!("[sys_getaddrinfo] nodename: {:?}, servname: {:?}", nodename, servname);
+    unsafe { ax_posix_api::sys_getaddrinfo(nodename, servname, hints, res) }
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_send(s: i32, mem: *const c_void, len: usize, flags: i32) -> isize {
+    info!("[sys_send] socket: {}, len: {}, flags: {}", s, len, flags);
+    ax_posix_api::sys_send(s, mem, len, flags)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_socket(domain: i32, type_: i32, protocol: i32) -> i32 {
+    info!(
+        "[sys_socket] domain: {}, type: {}, protocol: {}",
+        domain, type_, protocol
+    );
+    let domain = convert_family(domain);
+    ax_posix_api::sys_socket(domain, type_, protocol)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_connect(socket_fd: i32, name: *const sockaddr, namelen: socklen_t) -> i32 {
+    info!(
+        "[sys_connect] socket_fd: {}, namelen: {}",
+        socket_fd, namelen
+    );
+    info!("name: {:?}", unsafe { *name });
+    let mut name_converted = unsafe { *name };
+    name_converted.sa_family = convert_family((name_converted.sa_family >> 8) as _) as _;
+    ax_posix_api::sys_connect(socket_fd, &name_converted, namelen)
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_recv(socket: i32, buf: *mut u8, len: usize, flags: i32) -> isize {
+    info!(
+        "[sys_recv] socket: {}, len: {}, flags: {}",
+        socket, len, flags
+    );
+    ax_posix_api::sys_recv(socket, buf as _, len, flags)
+}

os/arceos/ulib/arceos-rust/lib/src/lib.rs
@@ -4,8 +4,9 @@
 extern crate alloc;
 
 mod syscall;
+mod interface;
 
-use ax_api::modules::ax_log::{ax_println, debug, info};
+use ax_api::modules::ax_log::{debug, info};
 use ax_runtime::ax_app_entry;
 
 unsafe extern "C" {

os/arceos/ulib/arceos-rust/lib/src/syscall.rs
@@ -1,9 +1,7 @@
-use alloc::alloc;
 use ax_api::modules::ax_hal::time::wall_time_nanos;
-use ax_api::modules::ax_log::{ax_println, error, info};
+use ax_api::modules::ax_log::{ax_println, info};
 use ax_api::sys::ax_terminate;
-use ax_posix_api::ctypes::{iovec, timespec};
-use core::alloc::Layout;
+use ax_posix_api::ctypes::timespec;
 use rand::rngs::SmallRng;
 use rand::{RngCore, SeedableRng};
 
@@ -28,36 +26,6 @@ pub fn sys_futex_wake(address: *mut u32, count: i32) -> i32 {
 }
 
 #[unsafe(no_mangle)]
-pub fn sys_malloc(size: usize, align: usize) -> *mut u8 {
-    info!("called sys_malloc with size {} and align {}", size, align);
-    if let Ok(layout) = Layout::from_size_align(size, align) {
-        unsafe { alloc::alloc(layout) }
-    } else {
-        core::ptr::null_mut()
-    }
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_free(ptr: *mut u8, size: usize, align: usize) {
-    info!("called sys_free");
-    if let Ok(layout) = Layout::from_size_align(size, align) {
-        unsafe { alloc::dealloc(ptr, layout) }
-    } else {
-        error!("sys_free called with invalid layout: size {}, align {}", size, align);
-    }
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_realloc(ptr: *mut u8, size: usize, align: usize, new_size: usize) -> *mut u8 {
-    info!("called sys_realloc");
-    if let Ok(layout) = Layout::from_size_align(size, align) {
-        unsafe { alloc::realloc(ptr, layout, new_size) }
-    } else {
-        core::ptr::null_mut()
-    }
-}
-
-#[unsafe(no_mangle)]
 pub fn sys_abort() -> ! {
     info!("called sys_abort");
     ax_terminate()
@@ -68,19 +36,6 @@ pub fn sys_exit(code: i32) -> ! {
     info!("called sys_exit with code {}", code);
     ax_println!("[ArceOS] Process exited with code {}", code);
     ax_terminate()
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_write(fd: i32, buf: *const u8, count: usize) -> isize {
-    info!("called sys_write");
-    ax_posix_api::sys_write(fd, buf as _, count)
-}
-
-#[unsafe(no_mangle)]
-pub fn sys_writev(fd: i32, iov: *const iovec, iovcnt: usize) -> isize {
-    // Placeholder implementation
-    info!("called sys_writev");
-    0
 }
 
 #[unsafe(no_mangle)]